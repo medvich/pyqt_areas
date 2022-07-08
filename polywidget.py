bb92af14b4dd516598984bfa7617078f2bb77273
Sergey <sergey66rt@gmail.com>
2022-07-08 17:36:23 +0300

    Added some more preparations
     - Some buttons and widgets removed as unnecessary;
     - All ready to work with pyqtgraph.

--- polywidget.py
+++ polywidget.py
@@ -12,9 +12,22 @@ UI_WIDGET_FILE = 'graphWidgetForm.ui'
 
 class PolyWidget(QtWidgets.QWidget):
 
+    DEFAULT_LINE_COLOR = (255, 0, 0, 255)
+    DEFAULT_LINE_WIDTH = 3
+    DEFAULT_LINE_STYLE = 'Solid'
+    DEFAULT_MARKER_COLOR = (0, 0, 255, 255)
+    DEFAULT_MARKER_SIZE = 10
+    DEFAULT_MARKER_STYLE = 'o'
+    DEFAULT_FILL_COLOR = (255, 255, 255, 255)
+
+    # ~~~ Инициализация и подключение сигналов ~~~ #
+
     def __init__(self, *args, **kw):
         super().__init__(*args, **kw)
         loadUi(UI_WIDGET_FILE, self)
+
+        # Добавим ключ, по которому будем определять, выбран ли какой-то объект в QListWidget или нет
+        self.selectedFlag = True
 
         # Подключим вывод в консоль о произведенных действиях (при необходимости)
         self.connectLogging(True)
@@ -26,9 +39,9 @@ class PolyWidget(QtWidgets.QWidget):
         self._init_displayData()
 
     @staticmethod
-    def connectLogging(log):
-        if log:
-            logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+    def connectLogging(status):
+        if status:
+            logging.basicConfig(format="%(asctime)s :    %(levelname)s :    %(message)s", level=logging.INFO)
 
     def connectSignals(self):
         self.addPolyPushButton.clicked.connect(self.polyAddition)
@@ -36,12 +49,34 @@ class PolyWidget(QtWidgets.QWidget):
         self.polyListWidget.itemChanged.connect(self.polyItemChangedEvent)
         self.polyListWidget.itemClicked.connect(self.polyItemSelectedEvent)
 
+        # Панель кастомизации полигонов (изначально деактивирована)
+        self.lineColorButtonWidget.sigColorChanged.connect(self.lineColorChanged)
+        self.markerColorButtonWidget.sigColorChanged.connect(self.markerColorChanged)
+        self.polyFillColorButtonWidget.sigColorChanged.connect(self.fillColorChanged)
+        self.lineStyleComboBox.activated.connect(self.lineStyleChanged)
+        self.markerStyleComboBox.activated.connect(self.markerStyleChanged)
+        self.lineWidthSpinBox.valueChanged.connect(self.lineWidthChanged)
+        self.markerSizeSpinBox.valueChanged.connect(self.markerSizeChanged)
+
     def _init_displayData(self):
         self.key_id = 0
         self.displayData = []
-        self.customPolygonStructure = namedtuple("customPolygonStructure", ["key_id", "name", "exterior", "interiors"])
+        self.customPolygonStructure = namedtuple(
+            "customPolygonStructure",
+            ["key_id",
+             "name",
+             "exterior",
+             "interiors",
+             "linecolor",
+             "linewidth",
+             "linestyle",
+             "markercolor",
+             "markersize",
+             "markerstyle",
+             "fillcolor"]
+        )
 
-    #######
+    # ~~~ Методы, работающие с элементами в polyListWidget ~~~ #
 
     def polyAddition(self):
         # Преобразуем новый полигон в Item, чтобы можно было с ним работать, как с QListWidgetItem
@@ -62,7 +97,19 @@ class PolyWidget(QtWidgets.QWidget):
         newPolygonAsItem.setFlags(newPolygonAsItem.flags() | QtCore.Qt.ItemIsEditable)
 
         # Создаем новый полигон как объект структуры customPolygonStructure
-        newPolygon = self.customPolygonStructure(newPolygonAsItem.data(1), newPolygonAsItem.text(), [], [])
+        newPolygon = self.customPolygonStructure(
+            newPolygonAsItem.data(1),
+            newPolygonAsItem.text(),
+            [],
+            [],
+            self.DEFAULT_LINE_COLOR,
+            self.DEFAULT_LINE_WIDTH,
+            self.DEFAULT_LINE_STYLE,
+            self.DEFAULT_MARKER_COLOR,
+            self.DEFAULT_MARKER_SIZE,
+            self.DEFAULT_MARKER_STYLE,
+            self.DEFAULT_FILL_COLOR
+        )
         assert type(newPolygon.name) is str and \
                type(newPolygon.exterior) in (list, np.ndarray) and \
                type(newPolygon.interiors) in (list, np.ndarray), "Wrong parameter or parameters type"
@@ -81,7 +128,9 @@ class PolyWidget(QtWidgets.QWidget):
         Данный метод реализует удаление выбранного Item'а из QListWidget'а и из displayArea
         """
 
-        # ОпределяемЮ какой сейчас Item selected и удаляем его из QListWidget'а
+        self.selectedFlag = True
+
+        # Определяем, какой сейчас Item selected и удаляем его из QListWidget'а
         selectedItem = self.polyListWidget.currentItem()
         self.polyListWidget.takeItem(self.polyListWidget.row(selectedItem))
 
@@ -93,10 +142,19 @@ class PolyWidget(QtWidgets.QWidget):
                 # Информация
                 logging.info(f"Ключ {selectedItem.data(1)}. Элемент {selectedItem.text()} удален")
 
-                # Disabl'им кнопку удаления если ни один элемент не selected во избежания лишних тыканий и
-                # выползания ошибок
-                if self.polyListWidget.currentItem() is None:
-                    self.deletePolyPushButton.setEnabled(False)
+                # Переопределяем новый выбранный элемент
+                newSelectedItem = self.polyListWidget.currentItem()
+
+                # Disabl'им кнопку удаления, кнопку addHole и меню редактирования, если ни один элемент не selected
+                # (во избежания лишних тыканий и выползания ошибок)
+                if newSelectedItem is None:
+                    self.setItemCustomizationButtonsActive(False)
+                    return
+
+                # И снова информация, так как при удалении selected Item стал предыдущий
+                logging.info(f"Ключ {newSelectedItem.data(1)}. Элемент {newSelectedItem.text()} выбран")
+
+                # Выходим, чтобы в первую очередь прервать дальнейший поиск по циклу
                 return
 
     def polyItemChangedEvent(self, item):
@@ -122,21 +180,109 @@ class PolyWidget(QtWidgets.QWidget):
                 return
 
     def polyItemSelectedEvent(self, item):
-        # Активируем кнопку удаления полигона
-        self.deletePolyPushButton.setEnabled(True)
+        # Добавим возможность Select и Deselect Item
+        if self._isItemSelected(item):
+            item.setSelected(True)
+            # Активируем все функции кастомизации области
+            self.setItemCustomizationButtonsActive(True)
+            self.fillItemCustomizationButtons(item)
+            return
+        self.setItemCustomizationButtonsActive(False)
 
-        # Активируем меню редактирования и информационное окно
-        self.treeWidget.setEnabled(True)
-        self.addExBoundPushButton.setEnabled(True)
-        self.lineColorButtonWidget.setEnabled(True)
-        self.markerColorButtonWidget.setEnabled(True)
-        self.polyFillColorButtonWidget.setEnabled(True)
-        self.lineStyleComboBox.setEnabled(True)
-        self.markerStyleComboBox.setEnabled(True)
-        self.lineWidthSpinBox.setEnabled(True)
-        self.markerWidthSpinBox.setEnabled(True)
+    # ~~~ Методы, обрабатывающие сигналы от панели кастомизации полигонов ~~~ #
 
-    #######
+    def lineColorChanged(self):
+        """
+        Метод, меняющий цвет линий выбранного полигона
+        """
+
+        index = self.findItemIndexInData(self.polyListWidget.currentItem())
+        self.displayData[index] = \
+            self.displayData[index]._replace(linecolor=self.lineColorButtonWidget.color(mode='byte'))
+
+        # Информация
+        logging.info(f"Ключ {self.displayData[index].key_id}. Цвет линий элемента {self.displayData[index].name} "
+                     f"изменен на {self.displayData[index].linecolor}")
+
+    def markerColorChanged(self):
+        """
+        Метод, меняющий цвет точек(узлов) выбранного полигона
+        """
+
+        index = self.findItemIndexInData(self.polyListWidget.currentItem())
+        self.displayData[index] = \
+            self.displayData[index]._replace(markercolor=self.markerColorButtonWidget.color(mode='byte'))
+
+        # Информация
+        logging.info(f"Ключ {self.displayData[index].key_id}. Цвет узлов элемента {self.displayData[index].name} "
+                     f"изменен на {self.displayData[index].markercolor}")
+
+    def fillColorChanged(self):
+        """
+        Метод, меняющий цвет заливки выбранного полигона
+        """
+
+        index = self.findItemIndexInData(self.polyListWidget.currentItem())
+        self.displayData[index] = \
+            self.displayData[index]._replace(fillcolor=self.polyFillColorButtonWidget.color(mode='byte'))
+
+        # Информация
+        logging.info(f"Ключ {self.displayData[index].key_id}. Цвет заливки элемента {self.displayData[index].name} "
+                     f"изменен на {self.displayData[index].fillcolor}")
+
+    def lineStyleChanged(self):
+        """
+        Метод, изменяющий стиль линий в зависимости от выбранного элемента в lineStyleComboBox
+        """
+
+        index = self.findItemIndexInData(self.polyListWidget.currentItem())
+        self.displayData[index] = \
+            self.displayData[index]._replace(linestyle=self.lineStyleComboBox.currentText())
+
+        # Информация
+        logging.info(f"Ключ {self.displayData[index].key_id}. Стиль линий элемента {self.displayData[index].name} "
+                     f"изменен на {self.displayData[index].linestyle}")
+
+    def markerStyleChanged(self):
+        """
+        Метод, изменяющий стиль точек (узлов) в зависимости от выбранного элемента в markerStyleComboBox
+        """
+
+        index = self.findItemIndexInData(self.polyListWidget.currentItem())
+        self.displayData[index] = \
+            self.displayData[index]._replace(markerstyle=self.markerStyleComboBox.currentText())
+
+        # Информация
+        logging.info(f"Ключ {self.displayData[index].key_id}. Стиль узлов элемента {self.displayData[index].name} "
+                     f"изменен на {self.displayData[index].markerstyle}")
+
+    def lineWidthChanged(self):
+        """
+        Метод, изменяющий толщину линий в зависимости от числа в lineWidthSpinBox
+        """
+
+        index = self.findItemIndexInData(self.polyListWidget.currentItem())
+        self.displayData[index] = \
+            self.displayData[index]._replace(linewidth=self.lineWidthSpinBox.value())
+
+        # Информация
+        logging.info(f"Ключ {self.displayData[index].key_id}. Толщина линий элемента {self.displayData[index].name} "
+                     f"изменена на {self.displayData[index].linewidth}")
+
+    def markerSizeChanged(self):
+        """
+        Метод, изменяющий размер точек (узлов) в зависимости от числа в markerSizeSpinBox
+        """
+
+        index = self.findItemIndexInData(self.polyListWidget.currentItem())
+        self.displayData[index] = \
+            self.displayData[index]._replace(markersize=self.markerSizeSpinBox.value())
+
+        # Информация
+        logging.info(f"Ключ {self.displayData[index].key_id}. Толщина линий элемента {self.displayData[index].name} "
+                     f"изменена на {self.displayData[index].markersize}")
+
+    # ~~~ Сопутствующие методы ~~~ #
 
     def displayDataNames(self):
         """
@@ -147,6 +293,76 @@ class PolyWidget(QtWidgets.QWidget):
         for poly in self.displayData:
             res.append(poly.name)
         return res
+
+    def _isItemSelected(self, item):
+        """
+        Метод проверяет, Selected ли Item
+        """
+        if item.isSelected() and self.selectedFlag:
+            self.selectedFlag = False
+            # Информация
+            logging.info(f"Ключ {item.data(1)}. Элемент {item.text()} выбран")
+            return True
+
+        item.setSelected(False)
+        self.selectedFlag = True
+        # Информация
+        logging.info(f"Ключ {item.data(1)}. Выбор с элемента {item.text()} снят")
+        return False
+
+    def setItemCustomizationButtonsActive(self, status):
+        # Активируем/деактивируем кнопку удаления полигона
+        self.deletePolyPushButton.setEnabled(status)
+
+        # Активируем/деактивируем кнопку добавления выреза
+        self.addHolePushButton.setEnabled(status)
+
+        # Активируем/деактивируем меню редактирования
+        self.lineColorButtonWidget.setEnabled(status)
+        self.markerColorButtonWidget.setEnabled(status)
+        self.polyFillColorButtonWidget.setEnabled(status)
+        self.lineStyleComboBox.setEnabled(status)
+        self.markerStyleComboBox.setEnabled(status)
+        self.lineWidthSpinBox.setEnabled(status)
+        self.markerSizeSpinBox.setEnabled(status)
+
+    def fillItemCustomizationButtons(self, item):
+        # Найдем selected Item в displayData
+        index = self.findItemIndexInData(item)
+        selectedItemFromData = self.displayData[index] if index is not None else None
+
+        # Если нашли (что хотелось бы...), то заполняем панель кастомизации, исходя из информации о полигоне
+        # в displayData
+        if selectedItemFromData is not None:
+            self.lineColorButtonWidget.setColor(selectedItemFromData.linecolor)
+            self.markerColorButtonWidget.setColor(selectedItemFromData.markercolor)
+            self.polyFillColorButtonWidget.setColor(selectedItemFromData.fillcolor)
+
+            assert selectedItemFromData.linestyle in \
+                   [self.lineStyleComboBox.itemText(i) for i in
+                    range(self.lineStyleComboBox.count())], "Default linestyle not in possible linestyle list"
+            self.lineStyleComboBox.setCurrentText(selectedItemFromData.linestyle)
+
+            assert selectedItemFromData.markerstyle in \
+                   [self.markerStyleComboBox.itemText(i) for i in
+                    range(self.markerStyleComboBox.count())], "Default markerstyle not in possible markerstyle list"
+            self.markerStyleComboBox.setCurrentText(selectedItemFromData.markerstyle)
+
+            self.lineWidthSpinBox.setValue(selectedItemFromData.linewidth)
+            self.markerSizeSpinBox.setValue(selectedItemFromData.markersize)
+        else:
+            raise Exception("Couldn't find selected item in displayData...")
+
+    def findItemIndexInData(self, item):
+        """
+        Метод ищет индекс Item'а в displayData и возвращает его или None,
+        если элемент не нашелся (надеюсь, такого не будет)
+        """
+
+        for i in range(len(self.displayData)):
+            if self.displayData[i].key_id == item.data(1):
+                return i
+        return None
 
     #...
 
